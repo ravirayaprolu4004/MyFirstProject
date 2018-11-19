1cd7c301d758f20b0620f9b002226b480df5534d
ravirayaprolu4004 <45154401+ravirayaprolu4004@users.noreply.github.com>
2018-11-18 18:35:06 -0600

    Add files via upload

---
+++ FirstProject.py
@@ -0,0 +1,8 @@
+Python 3.7.1 (v3.7.1:260ec2c36a, Oct 20 2018, 03:13:28) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ( "Ravi")
+Ravi
+>>> print ("This is my first python execution")
+This is my first python execution
+>>> 
